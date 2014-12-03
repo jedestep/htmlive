abe5535d9674ac4b573b5ee5faa8dd8b3d11e419
Jed Estep <aje@jhu.edu>
2014-12-02 23:08:45 -0500

    real time wow! SSE! so cool!

--- htmlive/server.py
+++ htmlive/server.py
@@ -1,24 +1,51 @@
-from flask import Flask, render_template
-from flask.ext.socketio import SocketIO,send,emit
+from flask import Flask, Response, render_template
+from time import sleep
 
+import os
 import sys
+import signal
+
+MONITOR_PATH = 'templates/out.html'
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+
+going = True
+
+def stopGoing(signum,frame):
+    going = False
+    exit(0)
+
+def evstream():
+    last_change = ''
+    while going:
+        try:
+            stats = os.stat(MONITOR_PATH)
+            if stats.st_ctime != last_change:
+                last_change = stats.st_ctime
+                yield 'data: refresh\n\n'
+                print 'heard about a change'
+            sleep(1)
+        except KeyboardInterrupt:
+            break
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@socketio.on('my event')
-def do_my_event(json):
-    print 'received event:', json
+@app.route('/show')
+def show():
+    return render_template("out.html")
 
-@socketio.on('_read')
-def do_read(json):
-    print 'input command now'
-    text = sys.stdin.readline()
-    emit('_write',{'html': text})
+@app.route('/stream')
+def stream():
+    return Response(evstream(),
+                    mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    socketio.run(app)
+    signal.signal(signal.SIGINT,stopGoing)
+    signal.signal(signal.SIGABRT,stopGoing)
+    signal.signal(signal.SIGQUIT,stopGoing)
+    signal.signal(signal.SIGHUP,stopGoing)
+    signal.signal(signal.SIGTERM,stopGoing)
+    app.debug = True
+    app.run(threaded=True)
